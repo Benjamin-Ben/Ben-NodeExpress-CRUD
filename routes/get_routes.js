98337218e2ddc5965a936a011eaf64cde26fb3f3
Benjamin-Ben <42369601+Benjamin-Ben@users.noreply.github.com>
2019-03-09 16:08:50 +0100

    Made the error handling a bit more readable

--- routes/get_routes.js
+++ routes/get_routes.js
@@ -18,7 +18,9 @@ module.exports = function (app) {
         get1: function ( urlName, sqlQuery1, viewTemplate, pageTitle ) {
             app.get( urlName, ( req, res, next ) => {
                 db.query(sqlQuery1, ( err, results1 ) => {
-                    if (err) { res.render('error_page', { err }); }
+                    if (err) { 
+                        res.render('error_page', { err }); 
+                    }
 
                     res.render(viewTemplate, { 'title': pageTitle, results1 });
                 });
